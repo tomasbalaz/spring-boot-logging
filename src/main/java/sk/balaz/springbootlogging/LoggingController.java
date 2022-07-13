5dd0474a0101bf636d9263a0578686c8fc23212b
Tomas Balaz <balaztomas0@gmail.com>
2022-07-13 13:08:36 +0200

    How to use logback.xml file for enable logback logging

--- src/main/java/sk/balaz/springbootlogging/LoggingController.java
+++ src/main/java/sk/balaz/springbootlogging/LoggingController.java
@@ -2,7 +2,6 @@ package sk.balaz.springbootlogging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
